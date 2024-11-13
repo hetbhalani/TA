77a3acc42b46f0b9cffc4e4c506d4638f6504975
hetbhalani <bhalanihet2006@gmail.com>
2024-11-13 22:23:35 +0530

    initial commit

---
+++ BCA-DS/LAB-2/series2.c
@@ -0,0 +1,22 @@
+//10. WAP to print sum of series 1 – 2 + 3 – 4 + 5 – 6 + 7 … n.
+
+#include<stdio.h>
+
+void main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int i = 1, sum = 0;
+
+    while(i <= n){
+        if(i % 2 == 0){
+            sum -= i;  // Subtract if i is even
+        } else {
+            sum += i;  // Add if i is odd
+        }
+        i++;
+    }
+
+    printf("%d", sum);
+}
